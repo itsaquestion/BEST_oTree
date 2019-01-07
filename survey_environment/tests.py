4b6a9cde065cac29c2a1018cdd32c642ab804137
itsaquestion <itsaquestion@gmail.com>
2019-01-07 18:57:28 +0800

    fix 1个博弈矩阵，survey_env不用再等

--- survey_environment/tests.py
+++ survey_environment/tests.py
@@ -7,5 +7,11 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (pages.Survey)
-
+        value = '1.非常不赞成'
+        yield (pages.Survey,
+               {'sur_q01': value, 'sur_q02': value, 'sur_q03': value, 'sur_q04': value, 'sur_q05': value, 'sur_q06': value,
+                'sur_q07': value, 'sur_q08': value,
+                'sur_q09': value, 'sur_q10': value, 'sur_q11': value, 'sur_q12': value, 'sur_q13': value, 'sur_q14': value,
+                'sur_q15': value, 'sur_q16': value,
+                'sur_q17': value, 'sur_q18': value, 'sur_q19': value,
+                })
